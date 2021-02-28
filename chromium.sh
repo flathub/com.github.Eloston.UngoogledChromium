bcd8249d0cfe3a9ac264e7efdb17f144f55fc9a0
rany <ranygh@riseup.net>
2021-02-28 15:50:37 +0200

    Disable beta version of this flatpak

--- chromium.sh
+++ chromium.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
 
-get_int32_property() {
-  gdbus call -e \
-    -d org.freedesktop.portal.Flatpak \
-    -o /org/freedesktop/portal/Flatpak \
-    -m org.freedesktop.DBus.Properties.Get \
-    org.freedesktop.portal.Flatpak "$1" \
-    | awk 'match($0, /uint32 ([0-9]+)/, m){print m[1];}'
-}
-
-# Check the portal version & make sure it supports expose-pids.
-if [[ $(get_int32_property version) -lt 4 || \
-      $(($(get_int32_property supports) & 1)) -eq 0 ]]; then
-  zenity --info --title='Chromium Flatpak' --no-wrap \
-    --text="$(< /app/share/flatpak-chromium/portal_error.txt)"
-  exit 1
-fi
-
-# Merge the flags.
-if [[ -f "$XDG_CONFIG_HOME/chromium-flags.conf" ]]; then
-  IFS=$'\n'
-  flags=($(grep -v '^#' "$XDG_CONFIG_HOME/chromium-flags.conf"))
-  unset IFS
-
-  set -- "${flags[@]}" "$@"
-fi
-
-flextop-init
-
-export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
-export CHROME_WRAPPER=$(readlink -f "$0")
-exec /app/chromium/chrome "$@"
+exec zenity --info --title='Chromium Flatpak' --no-wrap \
+  --text="\
+The 'beta' branch has been deprecated recently. If you want to use
+the latest version of ungoogled-chromium, upgrade to the 'stable'
+branch: 'app/com.github.Eloston.UngoogledChromium/x86_64/stable'"
--- get-latest-ungoogled.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -s https://github.com/Eloston/ungoogled-chromium/releases \
-	| grep '/Eloston/ungoogled-chromium/releases/tag/' \
-	| cut -d/ -f6 | head -1 | sed -e 's/\">$//g'
