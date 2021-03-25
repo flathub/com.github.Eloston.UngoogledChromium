b539d3c5ee2964d56235933f1b3480aeb2f47eb6
rany <ranygh@riseup.net>
2021-03-25 14:55:41 +0200

    Update widevine install script

--- widevine-install.sh
+++ widevine-install.sh
@@ -1,29 +1,30 @@
-#!/bin/bash -eux
+#!/usr/bin/env bash
 
-# Get latest WideVine Version
+# Exit on error and forbid unset variables
+set -eu
+
+# Get latest WideVine Version by getting last line in https://dl.google.com/widevine-cdm/versions.txt
 _widevine_ver="$(wget -qO- https://dl.google.com/widevine-cdm/versions.txt | tail -n1)"
 
-# Make temporary directory for download
-_mktemp_dir="$(mktemp -d)"
-cd "$_mktemp_dir"
-
-# Use the architecture of the current machine
+# Get the architecture of the current machine
 ARCH="$(uname -m)"
 case "$ARCH" in
-  x86_64) WIDEVINE_ARCH="x64";  CHROMIUM_ARCH="x64" ;;
-    i?86) WIDEVINE_ARCH="ia32"; CHROMIUM_ARCH="x86" ;;
-       *) echo "The architecture $ARCH is not supported." >&2 ; exit 1 ;;
+	x86_64) WIDEVINE_ARCH="x64"
+		CHROMIUM_ARCH="x64"
+		;;
+
+	*)	echo "The architecture $ARCH is not supported." >&2
+		exit 1
+		;;
 esac
 
-# Download and unzip widevine
-wget -c -O widevine.zip "https://dl.google.com/widevine-cdm/${_widevine_ver}-linux-${WIDEVINE_ARCH}.zip"
-unzip      widevine.zip
+# Download WideVine into a temporary file and use trap to delete it on exit
+widevine_zip="$(mktemp)"
+trap 'rm -f "${widevine_zip:?}"' EXIT
+wget -O "$widevine_zip" "https://dl.google.com/widevine-cdm/${_widevine_ver}-linux-${WIDEVINE_ARCH}.zip"
 
-# Install to UngoogledChromium flatpak
+# Install WideVine from zip file into UngoogledChromium flatpak
 _install_prefix="$HOME/.var/app/com.github.Eloston.UngoogledChromium/config/chromium/WidevineCdm/${_widevine_ver}"
-install -Dm644 libwidevinecdm.so   "${_install_prefix}/_platform_specific/linux_$CHROMIUM_ARCH/libwidevinecdm.so"
-install  -m644       manifest.json "${_install_prefix}/manifest.json"
-install  -m644        LICENSE.txt  "${_install_prefix}/LICENSE.txt"
-
-# Remove temporary directory
-cd / && rm -rf "$_mktemp_dir"
+unzip -p "$widevine_zip" libwidevinecdm.so | install -Dm644 "/proc/self/fd/0" "${_install_prefix}/_platform_specific/linux_$CHROMIUM_ARCH/libwidevinecdm.so"
+unzip -p "$widevine_zip" manifest.json     | install  -m644 "/proc/self/fd/0" "${_install_prefix}/manifest.json"
+unzip -p "$widevine_zip" LICENSE.txt       | install  -m644 "/proc/self/fd/0" "${_install_prefix}/LICENSE.txt"
