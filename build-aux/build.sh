b8e36b50ba919d23a8c18c5dbbdf3a07e38269b2
rany <ranygh@riseup.net>
2021-08-04 23:38:01 +0300

    Upgrade to UGC 92.0.4515.131

--- build-aux/build.sh
+++ build-aux/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -e
 
-. /usr/lib/sdk/node12/enable.sh
-. /usr/lib/sdk/openjdk11/enable.sh
+ln_overwrite_all() {
+	rm -rf "$2"
+	ln -s "$1" "$2"
+}
+
+# Link our verisons of Node and OpenJDK into Chromium so the build scripts will
+# use them. For OpenJDK especially, this is a workaround for:
+# https://bugs.chromium.org/p/chromium/issues/detail?id=1192875
+ln_overwrite_all /usr/lib/sdk/node12 third_party/node/linux/node-linux-x64
+ln_overwrite_all /usr/lib/sdk/openjdk11 third_party/jdk/current
+
 ninja -C out/ReleaseFree -j$FLATPAK_BUILDER_N_JOBS libffmpeg.so
-ninja -C out/Release -j$FLATPAK_BUILDER_N_JOBS chrome
+ninja -C out/Release -j$FLATPAK_BUILDER_N_JOBS chrome crashpad_handler
--- build-aux/install.sh
+++ build-aux/install.sh
@@ -4,7 +4,7 @@ mkdir -p /app/chromium
 
 pushd out/Release
 # Keep file names in sync with build_devel_flatpak.py
-for path in chrome icudtl.dat *.so *.pak *.bin *.png locales MEIPreload swiftshader; do
+for path in chrome crashpad_handler icudtl.dat *.so *.pak *.bin *.png locales MEIPreload swiftshader; do
 	cp -rv $path /app/chromium || true
 done
 popd
@@ -15,7 +15,7 @@ install -Dm 755 out/Release/libffmpeg.so /app/chromium/nonfree-codecs/lib/libffm
 for size in 24 48 64 128 256; do
 	install -Dvm 644 chrome/app/theme/chromium/product_logo_$size.png /app/share/icons/hicolor/${size}x${size}/apps/com.github.Eloston.UngoogledChromium.png;
 done
-install -Dvm 644 portal_error.txt -t /app/share/flatpak-chromium
+install -Dvm 644 cobalt.ini -t /app/etc
 install -Dvm 644 com.github.Eloston.UngoogledChromium.desktop -t /app/share/applications
 install -Dvm 644 com.github.Eloston.UngoogledChromium.metainfo.xml -t /app/share/metainfo
 install -Dvm 755 chromium.sh /app/bin/chromium
