612f41f6214195f3adcb71f2674c18eca25ddd17
rany <ranygh@riseup.net>
2022-02-03 20:55:22 +0200

    Update to Ungoogled Chromium 98.0.4758.80

--- get-latest-ungoogled.sh
+++ get-latest-ungoogled.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 tag=$(curl -w '%{redirect_url}\n' -o/dev/null -s https://github.com/Eloston/ungoogled-chromium/releases/latest | rev | cut -d/ -f1 | rev)
-printf '%s\n' "$tag"
-curl -s "https://github.com/Eloston/ungoogled-chromium/releases/tag/$tag" | grep -Eo 'data-hovercard-url=\".*' | grep -Eo '/commit/.*' |cut -d/ -f3
+commit=$(curl -s "https://github.com/Eloston/ungoogled-chromium/releases/tag/$tag" | grep -Eo 'data-hovercard-url=\".*' | grep -Eo '/commit/.*' |cut -d/ -f3)
+
+cat << EOF
+      - type: git
+        url: https://github.com/Eloston/ungoogled-chromium
+        tag: $tag
+        commit: $commit
+EOF
