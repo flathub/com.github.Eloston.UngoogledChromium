72b0c0be6e5e2f48ff0fb7d03ac994fdba9c6a0d
Ryan Gonzalez <rymg19@gmail.com>
2022-08-30 19:06:47 -0500

    Updates for Chromium 105
    Signed-off-by: rany <rany2@riseup.net>

--- build-aux/bootstrap.sh
+++ build-aux/bootstrap.sh
@@ -17,32 +17,28 @@ if [[ ! -d third_party/llvm-build/Release+Asserts/bin ]]; then
       --without-android --without-fuchsia
 fi
 
-mkdir -p out/Release
-cp /app/ugc/flags.gn out/Release/args.gn
-cat >> out/Release/args.gn <<-EOF
-	use_sysroot=false
-	use_lld=true
-	enable_nacl=false
-	blink_symbol_level=0
-	use_gnome_keyring=false
-	use_pulseaudio=true
-	clang_use_chrome_plugins=false
-	is_official_build=true
-	treat_warnings_as_errors=false
-	proprietary_codecs=true
-	ffmpeg_branding="Chrome"
-	is_component_ffmpeg=true
-	use_vaapi=true
-	enable_widevine=true
-	rtc_use_pipewire=true
-	rtc_link_pipewire=true
-	rtc_pipewire_version="0.3"
-	enable_hangout_services_extension=true
-	disable_fieldtrial_testing_config=true
-EOF
-tools/gn/bootstrap/bootstrap.py --skip-generate-buildfiles -j$FLATPAK_BUILDER_N_JOBS
+tools/gn/bootstrap/bootstrap.py -v --no-clean --gn-gen-args='
+    use_sysroot=false
+    use_lld=true
+    enable_nacl=false
+    blink_symbol_level=0
+    use_gnome_keyring=false
+    use_pulseaudio=true
+    clang_use_chrome_plugins=false
+    is_official_build=true
+    treat_warnings_as_errors=false
+    proprietary_codecs=true
+    ffmpeg_branding="Chrome"
+    is_component_ffmpeg=true
+    use_vaapi=true
+    enable_widevine=true
+    rtc_use_pipewire=true
+    rtc_link_pipewire=true
+    enable_hangout_services_extension=true
+    disable_fieldtrial_testing_config=true
+    use_system_libwayland=false
+'
 mkdir -p out/ReleaseFree
 cp out/Release{,Free}/args.gn
 echo -e 'proprietary_codecs = false\nffmpeg_branding = "Chromium"' >> out/ReleaseFree/args.gn
-out/Release/gn gen out/Release
 out/Release/gn gen out/ReleaseFree
