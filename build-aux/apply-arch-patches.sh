8e89c3d7007a93c082e50746b93883ba3ecc371e
rany <rany2@riseup.net>
2023-02-09 19:21:04 +0200

    Update Ungoogled Chromium to 110.0.5481.77

--- build-aux/apply-arch-patches.sh
+++ build-aux/apply-arch-patches.sh
@@ -10,8 +10,5 @@ patch -Rp1 -i /app/ugc-arch/REVERT-roll-src-third_party-ffmpeg-m106.patch
 # https://crbug.com/1314342
 patch -Np1 -i /app/ugc-arch/disable-GlobalMediaControlsCastStartStop.patch
 
-# https://crbug.com/angleproject/7582
-patch -Np0 -i /app/ugc-arch/angle-wayland-include-protocol.patch
-
 # Enable VAAPI on Wayland
 patch -Np1 -i /app/ugc-arch/0001-ozone-wayland-add-VA-API-support.patch
