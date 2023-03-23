903fb8ee72ec06dd57eab69ef74b407b427d1035
rany <rany2@riseup.net>
2023-03-23 19:29:42 +0200

    Update Ungoogled Chromium to 111.0.5563.110

--- build-aux/apply-arch-patches.sh
+++ build-aux/apply-arch-patches.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -ex
 
-# Revert ffmpeg roll requiring new channel layout API support
-# https://crbug.com/1325301
-patch -Rp1 -i /app/ugc-arch/REVERT-roll-src-third_party-ffmpeg-m102.patch
-# Revert switch from AVFrame::pkt_duration to AVFrame::duration
-patch -Rp1 -i /app/ugc-arch/REVERT-roll-src-third_party-ffmpeg-m106.patch
-
 # Disable kGlobalMediaControlsCastStartStop by default
 # https://crbug.com/1314342
 patch -Np1 -i /app/ugc-arch/disable-GlobalMediaControlsCastStartStop.patch
 
 # Enable VAAPI on Wayland
 patch -Np1 -i /app/ugc-arch/0001-ozone-wayland-add-VA-API-support.patch
+# https://github.com/ungoogled-software/ungoogled-chromium/issues/2228
+# https://aur.archlinux.org/cgit/aur.git/tree/?h=ungoogled-chromium
+patch -Np1 -i /app/ugc-arch/vaapi-add-av1-support.patch
