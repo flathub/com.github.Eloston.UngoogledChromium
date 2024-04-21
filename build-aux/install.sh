f8f6282ffc8a1f01ac67ff26459f4eff1cd68a1f
rany <ranygh@riseup.net>
2024-04-22 01:39:47 +0300

    Make use of rename-icon/rename-desktop-file/rename-appdata-file to avoid sed
    This makes our `build-aux/install.sh` fully in sync with the
    up-to-date `io.github.ungoogled_software.ungoogled_chromium` flatpak.
    
    Signed-off-by: rany <ranygh@riseup.net>

--- build-aux/install.sh
+++ build-aux/install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -ex
 
-fix-app-id() {
-	sed -e 's|io.github.ungoogled_software.ungoogled_chromium|com.github.Eloston.UngoogledChromium|g' "${@}"
-}
-
 mkdir -p /app/chromium
 
 pushd out/Release
@@ -18,9 +14,9 @@ popd
 install -Dm 755 out/ReleaseFree/libffmpeg.so /app/chromium/libffmpeg.so
 install -Dm 755 out/Release/libffmpeg.so /app/chromium/nonfree-codecs/lib/libffmpeg.so
 for size in 24 48 64 128 256; do
-	install -Dvm 644 "chrome/app/theme/chromium/linux/product_logo_${size}.png" "/app/share/icons/hicolor/${size}x${size}/apps/com.github.Eloston.UngoogledChromium.png";
+	install -Dvm 644 "chrome/app/theme/chromium/linux/product_logo_${size}.png" "/app/share/icons/hicolor/${size}x${size}/apps/io.github.ungoogled_software.ungoogled_chromium.png";
 done
 install -Dvm 644 cobalt.ini -t /app/etc
-fix-app-id /app/ugc/io.github.ungoogled_software.ungoogled_chromium.desktop | install -Dvm 644 /dev/stdin /app/share/applications/com.github.Eloston.UngoogledChromium.desktop
-fix-app-id /app/ugc/io.github.ungoogled_software.ungoogled_chromium.metainfo.xml | install -Dvm 644 /dev/stdin /app/share/metainfo/com.github.Eloston.UngoogledChromium.metainfo.xml
+install -Dvm 644 /app/ugc/io.github.ungoogled_software.ungoogled_chromium.desktop -t /app/share/applications
+install -Dvm 644 /app/ugc/io.github.ungoogled_software.ungoogled_chromium.metainfo.xml -t /app/share/metainfo
 install -Dvm 755 chromium.sh /app/bin/chromium
