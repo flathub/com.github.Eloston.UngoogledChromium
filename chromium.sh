6b930ceeb6e03ea18ec5aadd00fa53c08925ae95
rany <ranygh@riseup.net>
2024-05-23 16:21:53 +0300

    Remove trailing period in heading and don't use hyperlinks in message
    If hyperlinks were used, it will just open up in the user's default
    browser which might be UGC. Fix that by just showing the link as is.
    
    Also remove unsupported markup in README like <small> and use markdown's
    inline formatting for code formatting (one backtick instead of the HTML
    equivalent).
    
    Signed-off-by: rany <ranygh@riseup.net>

--- chromium.sh
+++ chromium.sh
@@ -3,7 +3,7 @@
 exec zenity --info --title="Ungoogled Chromium Flatpak" --width=600 \
   --text="<b><big><big><big>URGENT PLEASE READ THIS</big></big></big></b>
 
-<b><big><big>YOU WILL NOT LOSE YOUR DATA BY UPDATING.</big></big></b>
+<b><big><big>YOU WILL NOT LOSE YOUR DATA BY UPDATING</big></big></b>
 
 <big>You <b>MUST</b> update to the new package name <tt><small>io.github.ungoogled_software.ungoogled_chromium</small></tt> to keep using Ungoogled Chromium.</big>
 
@@ -21,4 +21,10 @@ Updating to rebased version
 
 Changes complete.
 </small></tt>
-If you need any assistance or have any questions, feel free to ask in the Ungoogled Chromium Matrix room at <a href='https://matrix.to/#/#ungoogled-chromium:matrix.org'>#ungoogled-chromium:matrix.org</a> or the <a href='https://github.com/ungoogled-software/ungoogled-chromium-flatpak/issues'>GitHub issue tracker</a>."
+If you need any assistance or have any questions, feel free to ask in the Ungoogled Chromium Matrix room at:
+
+https://matrix.to/#/#ungoogled-chromium:matrix.org
+
+or the GitHub issue tracker:
+
+https://github.com/ungoogled-software/ungoogled-chromium-flatpak/issues"
